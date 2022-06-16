0e08d9233c2f20130890d3b18e153f58455af3cb
Joshua Tan <josh.tan2002@gmail.com>
2022-06-16 21:42:40 +0800

    Changes
    I decided I will just calculate the MA on my own and the EMA. It will require 5 data points before hand and I will just manually input it in. The later improvement will have it wait 5 minutes and every minute it will calculate the MA and then start trading

--- algotrade.py
+++ algotrade.py
@@ -3,7 +3,7 @@ from config import *
 import requests,json
 import time
 from datetime import datetime
-
+import pytz
 APCA_API_BASE_URL="https://paper-api.alpaca.markets/"
 ORDERS_URL = APCA_API_BASE_URL+"v2/orders"
 ACCOUNT_URL = APCA_API_BASE_URL+"v2/account"
@@ -15,7 +15,7 @@ headers = {
 
 POLYGONAPI  = polygonAPI
 POLYGON_URL = "https://api.polygon.io/v2/aggs/ticker/SPY/range/1/day/2020-06-01/2020-06-17?apiKey=" + POLYGONAPI
-RAPIDAPI_URL = "https://twelve-data1.p.rapidapi.com/"
+RAPIDAPI_URL = "https://twelve-data1.p.rapidapi.com/price"
 ALPHA_VENTURE_URL = "https://alpha-vantage.p.rapidapi.com/query"
 rapid_headers = {
 	"X-RapidAPI-Host": "twelve-data1.p.rapidapi.com",
@@ -87,45 +87,43 @@ def calculate_EMA(timeperiod):
     return response["ema"]
 
 def calculate_MA(timeperiod):
+
     #take the time period of minutes and calculate the avg from adding up all the prices
-    #does not give the correct MA 
-    # querystring = {"symbol":"SPY","interval":"1min","format":"json","series_type":"open","ma_type":"SMA","outputsize":"30","time_period": str(timeperiod)}
-    # response = requests.request("GET", RAPIDAPI_URL+"ma", headers= rapid_headers, params=querystring)
-    # response = json.loads(response.text)["values"][0]
-    # print("MA time is : "  + response["datetime"])
-    querystring = {"symbol":"SPY","function":"TIME_SERIES_INTRADAY","interval":"1min","output_size":"compact","datatype":"json"}
-    headers = {
-	"X-RapidAPI-Key": rapidAPI,
-	"X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    response = json.loads(response.text)["Time Series (1min)"]["1. open:"]    
-    return response["ma"]
+    [103.72,104.13,104.26,104.01,103.54]
+    querystring = {"symbol":"AMZN","outputsize":"30","format":"json"}
+    response = requests.request("GET", RAPIDAPI_URL, headers= rapid_headers, params=querystring)
+    response = json.loads(response.text)["price"]
+    return response
 
 marketopen = False
 buying = True
 orderID = None
-EMA = calculate_EMA(5)
+# EMA = calculate_EMA(5)
 MA = calculate_MA(5)
 print(MA)
-print(EMA)
-while marketopen:
+# print(MA)
+# print(EMA)
+# while marketopen:
 
-    if buying and orderID is None:
-        if EMA > MA:
-            response = buy_order('SPY', 1,"buy","market",'gtc')
-            buyID = response['id']
-            print('submitted buy request')
-            buying = False
-    else:
-        # Now we want to sell the stock when the conditions are met
-        if EMA < MA:
-            response = buy_order('SPY',1,"sell","market","gtc")
-            print('submitted sell request')
+#     if buying and orderID is None:
+#         if EMA > MA:
+#             response = buy_order('SPY', 1,"buy","market",'gtc')
+#             buyID = response['id']
+#             print('submitted buy request')
+#             buying = False
+#     else:
+#         # Now we want to sell the stock when the conditions are met
+#         if EMA < MA:
+#             response = buy_order('SPY',1,"sell","market","gtc")
+#             print('submitted sell request')
 
-    #waits 20 seconds then will check my order if the buyID has been sold or bought
-    print("waiting 20 seconds")
-    time.sleep(20000)
-    EMA,MA,buying,orderID = updateStats(EMA,MA,buying,orderID)
+#     #waits 20 seconds then will check my order if the buyID has been sold or bought
+#     print("waiting 20 seconds")
+#     time.sleep(20000)
+#     EMA,MA,buying,orderID = updateStats(EMA,MA,buying,orderID)
     
+
+
+
+
 
